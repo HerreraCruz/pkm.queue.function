347619e42abdd8eee9daae5f15c63e8b9ce57103
Carlos Julian Amaya Vasquez <carlos1305vasquez@gmail.com>
2025-04-24 01:53:37 -0600

    Adding load pokemons stats and abilities

--- function_app.py
+++ function_app.py
@@ -34,7 +34,9 @@ def QueueTriggerPokeReport(azqueue: func.QueueMessage):
 
     request_info = get_request(id)
     pokemons = get_pokemons(request_info[0]["type"])
-    pokemon_bytes = generate_csv_to_blob(pokemons)
+    pokemons_with_stats = get_pokemons_stats(pokemons)
+
+    pokemon_bytes = generate_csv_to_blob(pokemons_with_stats)
     blob_name = f"poke_report_{id}.csv"
     upload_csv_to_blob( blob_name, csv_data=pokemon_bytes)
     logger.info(f"Archivo CSV subido a Blob Storage: {blob_name}")
@@ -67,6 +69,29 @@ def get_pokemons(type: str) -> dict:
     pokemon_entries = data.get("pokemon", [])
 
     return [ p["pokemon"] for p in pokemon_entries]
+
+# Obtiene las estadisticas de cada pokemon
+def get_pokemons_stats(pokemons) -> list:
+    pokemons_with_stats = []
+    for pokemon in pokemons:
+        try:
+            pokemon_info = requests.get(pokemon["url"], timeout=3000)
+            pokemon_info.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
+            data_info = pokemon_info.json()
+
+            stats = {stat["stat"]["name"]: stat["base_stat"] for stat in data_info["stats"]}
+            abilities = {ability["ability"]["name"] for ability in data_info["abilities"]}
+            
+            pokemon_with_stats = {**pokemon, **stats, "abilities": ", ".join(abilities)}
+            pokemons_with_stats.append(pokemon_with_stats)
+        except requests.exceptions.RequestException as e:
+            logger.error(f"Error fetching data for Pokemon {pokemon['name']}: {e}")
+        except KeyError as e:
+            logger.error(f"Missing expected data for Pokemon {pokemon['name']}: {e}")
+        except Exception as e:
+            logger.error(f"Unexpected error for Pokemon {pokemon['name']}: {e}")
+
+    return pokemons_with_stats
 
 #transforma la lista de los pokemones a un csv
 def generate_csv_to_blob( pokemon_list ) -> bytes:
